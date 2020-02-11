56e723d3262cc10bb4cf59f46497769216dc7d7b
unknown <stefan.pavlovic.serbia@gmail.com>
2020-02-11 23:39:20 +0100

    Added the exercise: variable-operators-types.js

---
+++ exercises/variables-operators-types/variables-operators-types.js
@@ -0,0 +1,134 @@
+// Delete the comments per task, so you can see the results in the console. //
+
+
+//////////////////////////////// TASK 1: ////////////////////////////////
+
+// var firstNumber = 10;
+// var secondNUMBER = 15;
+// var 3rdNumber = 5;
+// var !4thNumber = 11;
+
+// console.log(firstNumber, secondNumber, 3rdNumber, !4thNumber); 
+
+//It will show the error: First variable - var firstNumber is OK;
+                        //Second variable - var secondNUMBER is not spelled correctly in console.log();
+                        //Third variable - var 3rdNumber can't start with the number;
+                        //Forth variable - var !4thNumber, variable can't start with exclamation mark (!);
+                        // Conclusion - Variables can start with: lowercase letter, dollar sign ($) and bottom line (_).
+
+
+//////////////////////////////// TASK 2: ////////////////////////////////
+
+// var myMood = "I am feeling happy while learning new things about JavaScript!"
+
+// console.log(myMood);
+
+
+//////////////////////////////// TASK 3: ////////////////////////////////
+
+// var myName = "Stefan";
+// var myLastName = "Pavlovic";
+// var myTown = "Arandjelovac";
+
+// var myBio = "My name is " + myName + " and my last name is " + myLastName + ". I am coming from town called " + myTown + ".";
+
+// console.log(myBio);
+
+
+//////////////////////////////// TASK 4: ////////////////////////////////
+
+// var a = 10; 
+// var b = 15;
+// var c = 7;
+// var d = 21;
+
+// var add = a + b + c + d;
+// var subtract = (d - a) - (15 - 7);
+// var multiply = a * b * c * d;
+// var divide = (a + b) / (c + d);
+// var combinedOperators = (c + d) * (b - a) / 2;
+
+// console.log(combinedOperators); //choose a variable name and put into console.log to see the results.
+
+
+//////////////////////////////// TASK 5: ////////////////////////////////
+
+// var oneKilogram = 1000; //1000g
+// var weightOfBananas = 1.2 //1.2kg
+
+// var bananasInGrams = oneKilogram * weightOfBananas;
+
+// console.log("The weight of bananas in grams is: " + bananasInGrams + " grams.");
+
+
+//////////////////////////////// TASK 6: ////////////////////////////////
+//First part of the assignment:
+
+// var minuteInSeconds = 60; //One minute in seconds
+// var minutesLate = 5;
+// var extraWaitingTime = minuteInSeconds * minutesLate;
+
+// console.log("We will wait for our friend Anna " + extraWaitingTime + " for seconds.");
+
+
+//Second part of the assignment:
+
+// var minuteInSeconds = 60; //One minute in seconds
+// var minutesLate = 30;
+
+// var extraWaitingTime = minuteInSeconds * minutesLate;
+
+// console.log("We will wait for our friend Tomm " + extraWaitingTime + " for seconds.");
+
+//////////////////////////////// TASK 7: ////////////////////////////////
+
+// var articleSizeInKb = 98; 
+// var usbSizeInMb = 4 * 1024;
+// var usbSizeInKb = usbSizeInMb * 1024;
+
+// console.log(usbSizeInKb);
+
+
+//////////////////////////////// TASK 8: ////////////////////////////////
+//Skipped in assignment.
+
+
+//////////////////////////////// TASK 9: ////////////////////////////////
+// a = 10;
+// b = 16;
+// c = 29;
+// d = 4;
+
+// a *= 2; //Same as: a = a * 2;
+// b += 3; //Same as: b = b + 3;
+// c /= 5; //Same as: c = c / 5;
+// d -= 7; //Same as: d = d - 7;
+
+// console.log(a, b, c, d);
+
+
+//////////////////////////////// TASK 10: ////////////////////////////////
+
+// var myName = "Stefan";
+// var $myAge = 27;
+// var _doIhaveAcat = false;
+// //The only 3 ways to start a variable.
+
+// console.log(typeof myName, typeof $myAge, typeof _doIhaveAcat);
+
+
+//////////////////////////////// TASK 11: ////////////////////////////////
+
+// console.log(typeof "number", typeof true, typeof null, typeof "false", typeof 56);
+
+
+//////////////////////////////// TASK 12: ////////////////////////////////
+
+// var myLastName = "Pavlovic";
+// var myFirstName = "Stefan";
+// var myYearOfBirth = 1992;
+// var myDayofBirth = 21;
+// var myMonthOfBirth = 10;
+// var myNumber = 10;
+
+// console.log (typeof myYearOfBirth === "number"); //Change the variable to see if it is true or false. Boolean = true or false.
