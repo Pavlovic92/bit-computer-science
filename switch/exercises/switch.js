ab95fe9cb49f03209bd15b5a453f75a50266c677
BIT Student <classroom@bgit.rs>
2020-02-12 17:55:40 +0100

    Added some new files

---
+++ switch/exercises/switch.js
@@ -0,0 +1,243 @@
+///////////////////// TASK 1: ////////////////////////
+
+// var day = 3;
+
+// var result = ""
+
+// switch (day) {
+//     case 1:
+//         result = "Monday";
+//         break;
+//     case 2:
+//         result = "Tuesday";
+//         break;
+//     case 3:
+//         result = "Wednesday";
+//         break;
+//     case 4:
+//         result = "Thursday";
+//         break;
+//     case 5:
+//         result = "Friday";
+//         break;
+//     case 6:
+//         result = "Saturday";
+//         break;
+//     case 7:
+//         result = "Sunday";
+//         break;
+
+//     default:
+//         result = "Error";
+//         break;
+
+// }
+
+// console.log(result);
+
+
+///////////////////// TASK 2: ////////////////////////
+
+// var day = 8; //If we type something besides 1-7, it will result in "Input must be a number between 1 and 7."
+// var result = ""
+
+// switch (day) {
+//     case 1:
+//         result = "Monday";
+//         break;
+//     case 2:
+//         result = "Tuesday";
+//         break;
+//     case 3:
+//         result = "Wednesday";
+//         break;
+//     case 4:
+//         result = "Thursday";
+//         break;
+//     case 5:
+//         result = "Friday";
+//         break;
+//     case 6:
+//         result = "Saturday";
+//         break;
+//     case 7:
+//         result = "Sunday";
+//         break;
+
+//     default:
+//         result = "Input must be a number between 1 and 7";
+//         break;
+// }
+
+// console.log(result);
+
+
+///////////////////// TASK 3: ////////////////////////
+
+// var day = 6; //If we type something besides 1-7, it will result in "Input must be a number between 1 and 7."
+// var result = ""
+
+// switch (day) {
+//     case 1:
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//         result = "It\’s weekday!"
+//         break;
+//     case 6:
+//     case 7:
+//         result = "It\s weekend!"
+//         break;
+
+//     default:
+//         result = "Input must be number between 1 and 7";
+//         break;
+// }
+
+// console.log(result);
+
+// ///////////////////// TASK 4: ////////////////////////
+
+// var month = 4; //If we type something besides 1-12, it will result in "Input must be a number between 1 and 12."
+// var result = ""
+
+// switch (month) {
+//     case 1:
+//         result = "January";
+//         break;
+//     case 2:
+//         result = "February";
+//         break;
+//     case 3:
+//         result = "March";
+//         break;
+//     case 4:
+//         result = "April";
+//         break;
+//     case 5:
+//         result = "May";
+//         break;
+//     case 6:
+//         result = "June";
+//         break;
+//     case 7:
+//         result = "July";
+//         break;
+//     case 8:
+//         result = "August";
+//         break;
+//     case 9:
+//         result = "September";
+//         break;
+//     case 10:
+//         result = "October";
+//         break;
+//     case 11:
+//         result = "November";
+//         break;
+//     case 12:
+//         result = "December";
+//         break;
+
+//     default:
+//         result = "Input must be a number between 1 and 12.";
+//         break;
+// }
+
+// console.log(result);
+
+
+// ///////////////////// TASK 5: ////////////////////////
+
+// var month = 12; //If we type something besides 1-12, it will result in "Input must be a number between 1 and 12."
+// var result = ""
+
+// switch (month) {
+
+//     case 3:
+//     case 4:
+//     case 5:
+//         result = "spring";
+//         break;
+
+//     case 6:
+//     case 7:
+//     case 8:
+//         result = "summer";
+//         break;
+
+//     case 9:
+//     case 10:
+//     case 11:
+//         result = "autumn";
+//         break;
+
+//     case 12:
+//     case 1:
+//     case 2:
+//         result = "winter";
+//         break;
+
+//     default:
+//         result = "Input must be a number between 1 and 12.";
+//         break;
+// }
+
+// console.log(result);
+
+
+// ///////////////////// TASK 6: ////////////////////////
+
+
+// var grade = "G"; //If we type something besides A, B, C, D and F, it will result in "Unknown grade."
+// var result = ""
+
+// switch (grade) {
+//     case "A":
+//         result = "Good job!";
+//         break;
+//     case "B":
+//         result = "Pretty Good!";
+//         break;
+//     case "C":
+//         result = "Passed!";
+//         break;
+//     case "D":
+//         result = "Not so good!";
+//         break;
+//     case "F":
+//         result = "Failed";
+//         break;
+
+//     default:
+//         result = "Unknown grade.";
+//         break;
+// }
+
+// console.log(result);
+
+///////////////////// TASK 7: ////////////////////////
+
+
+
+// var city = "Berlin"; //If we type something besides 1-12, it will result in "Input must be a number between 1 and 12."
+// var result = ""
+
+// switch (city) {
+
+//     case "Berlin":
+//     case "Frankfurt":
+//     case "Munich":
+//     case "Hamburg":
+//     case "Dresden":
+//     case "Augsburg":
+//         result = "Germany";
+//         break;
+
+//     default:
+//         result = "Input must be a number between 1 and 12.";
+//         break;
+// }
+
+// console.log(result);
