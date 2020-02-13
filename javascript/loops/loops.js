549c877b7475de4bb08a00434382612d67050fdb
unknown <stefan.pavlovic.serbia@gmail.com>
2020-02-13 23:59:12 +0100

    Finished some of the exercises

---
+++ javascript/loops/loops.js
@@ -0,0 +1,63 @@
+// Delete the comments per task, so you can see the results in the console. //
+
+/////////////////////// PRACTICE ///////////////////////
+
+// var number = 1;
+// var maxNumber = 10;
+
+// while (number <= maxNumber) {
+//     console.log("number is : " + number);
+//     var number = number + 1;
+// }
+
+
+//////////////////////////////////////////////
+// var minNumber = 1;
+// for (var number = 10; number >= minNumber; number--) {
+//     console.log("Number is: " + number);
+// }
+
+
+//////////////////////////////////////////////
+// for (var x = 0; x <= 10; x++) {
+//     console.log(x * x);
+// }
+
+//////////////////////////////////// TASK 1: ////////////////////////////////////
+
+// for (number = 0; number < 15; number++) {
+//     if (number % 2 === 0) {
+//         console.log("Number " + number + " is even.");
+//     } else {
+//         console.log("Number " + number + " is odd.");
+//     }
+
+// }
+
+
+///////////////////////////////// TASK 2: ////////////////////////////////////////
+
+// var result = 0;
+
+// for (var number = 1; number < 1000; number++) {
+//     if (number % 3 === 0 && number % 5 === 0) {
+//         result = result + number;
+//     }
+// }
+// console.log(result)
+
+
+///////////////////////////////// TASK 3: ////////////////////////////////////////
+
+// var result = 0;
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for (var index = 0; index < array.length; index++) {
+    
+//     result = result + array[index];
+// }
+
+// console.log(result);
+
+
+///////////////////////////////// TASK 4: ////////////////////////////////////////
