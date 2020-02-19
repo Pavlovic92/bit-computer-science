7c43583505c76a841d91d34c15f2a576f7f28cbd
BIT Student <classroom@bgit.rs>
2020-02-19 17:55:23 +0100

    Uploading exercise functions-2.js

---
+++ javascript/functions/exercise/functions-2.js
@@ -0,0 +1,79 @@
+////////////////////////// TASK 1: ////////////////////////////
+// function checkString(myString) {
+//     if (typeof myString === "string") {
+//         var printRes = true;
+//     } else {
+//         var printRes = false;
+//     } 
+//       return printRes;
+// }
+
+// var result = checkString(12);
+// console.log(result);
+
+
+////////////////////////// TASK 2: ////////////////////////////
+
+// function checkIfBlank(a) {
+//     if (typeof a !== "string") {
+//         return false;
+//     }
+
+//     for (var i = 0; i < a.length; i++) {
+//         if (a[i] !== " ") {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+// var result = checkIfBlank("");
+// console.log(result);
+
+
+////////////////////////// TASK 3: ////////////////////////////
+
+// function concatString(a) {
+//     var myString = "";
+//     for (var i = 0; i < a; i++) {
+//         myString += "Ha";
+//     }
+//     return myString;
+// }
+
+// var x = concatString(10);
+// console.log(x);
+
+
+////////////////////////// TASK 4: ////////////////////////////
+
+// function countNumberOfLetters(letter, text) {
+//     var res = 0;
+//     for (var i = 0; i < text.length; i++) {
+//         if (text[i] === letter) {
+//             res++;
+//         }
+//     }
+//     return res;
+// }
+// var x = countNumberOfLetters("n", "My random string");
+// console.log(x);
+
+
+////////////////////////// TASK 5: ////////////////////////////
+
+// function findPosition(letter, text) {
+//     for (var i = 0; i < text.length; i++) {
+//         if (text[i] === letter) {
+//             return i + 1;
+//         }
+//     }
+//     return -1;
+// }
+// var result = findPosition("r", "My random number");
+// console.log(result);
+
+
+////////////////////////// TASK 6: ////////////////////////////
+
+
