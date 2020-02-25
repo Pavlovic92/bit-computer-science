c5621efa66188098165f619f3b0ce17d58519e09
BIT Student <classroom@bgit.rs>
2020-02-25 18:10:35 +0100

    Uploading objects exercise

---
+++ javascript/functions/exercise/functions-4.js
@@ -0,0 +1,10 @@
+////////////// Task 1 //////////////
+var e = 3;
+var a = [5, -4.2, 3, 7];
+var result = "no";
+for (var i = 0; i < a.length; i++) {
+    if (e === a[i]) {
+        result = "yes";
+    }
+}
+console.log(result);
