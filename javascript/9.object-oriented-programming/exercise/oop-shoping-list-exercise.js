9b8df17517d72fe72d13322a826f02dcbc0ccbbf
unknown <stefan.pavlovic.serbia@gmail.com>
2020-03-09 00:28:27 +0100

    Started doing Shoping List exercise

---
+++ javascript/9.object-oriented-programming/exercise/oop-shoping-list-exercise.js
@@ -0,0 +1,66 @@
+/////////////
+'use strict';
+/////////////
+
+(function () {
+
+
+    //////////////////// PRODUCT //////////////////////    
+    function Product(name) {
+        this.name = name;
+
+        this.id = function () {
+            return Math.floor(Math.random() * 90000) + 10000;
+        }
+
+        this.price = function () {
+            return Math.floor(Math.random() * 100000) / 100;
+        }
+
+        this.getInfo = function () {
+
+            var firstAndLastLetter = '' + this.name[0] + this.name[this.name.length - 1].toUpperCase();
+
+            return '"' + this.name + '"' + ' -> ' + firstAndLastLetter + this.id() + ', ' + this.name + ', ' + this.price();
+        }
+    }
+
+    var bananaExpirationDate = new Date('03/10/2020');
+
+    var Banana = new Product('Banana', bananaExpirationDate);
+
+
+    //console.log(Banana.getInfo());
+
+    ////////////////// SHOPPING BAG /////////////////
+
+    function ShoppingBag() {
+        this.product = [];
+
+        this.todaysDate = new Date();
+
+        if (this.todaysDate < bananaExpirationDate) {
+            this.addProduct = function (product) {
+                this.product.push(product)
+                console.log(this.product);
+
+            }
+        }
+    }
+
+    var createShoppingBag = new ShoppingBag();
+
+    createShoppingBag.addProduct(Banana);
+
+
+
+
+
+
+
+
+
+
+
+
+})(); //Closing tag of anonymous immediately-invoked function                                                            
